c6723809a23dd621055c4f5015117703f042a9e4
Joao-Junior <joaojunior.b22@gmail.com>
2025-06-06 19:04:51 -0300

    ci: :construction_worker: setup fake api for frontend with mockoon

--- frontend/src/app/(store)/layout.tsx
+++ frontend/src/app/(store)/layout.tsx
@@ -1,11 +1,11 @@
 import Header from '@/components/header'
 import { CartProvider } from '@/context/cart-context'
 import { ReactNode } from 'react'
-import { useUserStore } from '@/context/stores/user-store'
+// import { useUserStore } from '@/context/stores/user-store'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
-  const user = useUserStore((state) => state.user)
-  console.log(user)
+  // const user = useUserStore((state) => state.user)
+  // console.log(user)
 
   return (
     <CartProvider>
