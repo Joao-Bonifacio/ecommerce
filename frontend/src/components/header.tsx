bc6afd06921d64015c843280d397054fabd09d28
Joao-Junior <joaojunior.b22@gmail.com>
2025-06-07 02:47:04 -0300

    style: :lipstick: add darkmode

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -3,8 +3,12 @@ import Link from 'next/link'
 import CartWidget from './cart-widget'
 import SearchForm from './search-form'
 import { Suspense } from 'react'
+import { cookies } from 'next/headers'
+import { ThemeToggle } from './theme-toggle'
 
-export default function Header() {
+export default async function Header() {
+  const token = (await cookies()).get('access_token')
+
   return (
     <header className="flex items-center justify-between !p-4">
       <div className="flex items-center gap-5">
@@ -21,17 +25,20 @@ export default function Header() {
         <CartWidget />
 
         <div className="w-px h-4 bg-zinc-700" />
+        <ThemeToggle />
 
-        <Link href="/" className="flex items-center gap-2 hover:underline">
-          <span className="text-md">Account</span>
-          <Image
-            src="https://github.com/Joao-Bonifacio.png"
-            className="h-7 w-7 rounded-full"
-            width={24}
-            height={24}
-            alt=""
-          />
-        </Link>
+        {token && (
+          <Link href="/" className="flex items-center gap-2 hover:underline">
+            <span className="text-md">Account</span>
+            <Image
+              src="https://github.com/Joao-Bonifacio.png"
+              className="h-7 w-7 rounded-full"
+              width={24}
+              height={24}
+              alt=""
+            />
+          </Link>
+        )}
       </div>
     </header>
   )
